8164747835c838a86fb9d0b0c3e90a9cfbefb745
Vishal Ubale <ubale.vishal50@gmail.com>
2019-08-12 13:04:16 +0530

    ADD

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
-import ipdb
-ipdb.set_trace()
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
